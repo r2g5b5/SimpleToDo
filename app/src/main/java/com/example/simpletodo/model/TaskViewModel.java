8d478144a68f42de0f64b6b9f0511e390edd3e95
r2g5b5 <programmer4906@gmail.com>
2021-05-11 22:41:19 +0430

    vx

--- app/src/main/java/com/example/simpletodo/model/TaskViewModel.java
+++ app/src/main/java/com/example/simpletodo/model/TaskViewModel.java
@@ -4,14 +4,41 @@ import android.app.Application;
 
 
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.simpletodo.data.ToDoRepository;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public class TaskViewModel extends AndroidViewModel {
+    private ToDoRepository repository;
+    private final LiveData<List<Task>> allTasks;
+
 
     public TaskViewModel(@NotNull Application application) {
         super(application);
+        repository=new ToDoRepository(application);
+        allTasks=repository.getAllTasks();
 
+    }
+
+
+    public LiveData<List<Task>> getAllTasks(){
+        return allTasks;
+    }
+    public LiveData<Task> getTask(int id){
+        return repository.getTask(id);
+    }
+    public void insertTask(Task task){
+        repository.insertTask(task);
+    }
+    public void updateTask(Task task){
+        repository.updateTask(task);
+    }
+    public void deleteTask(Task task){
+        repository.deleteTask(task);
     }
 
 
